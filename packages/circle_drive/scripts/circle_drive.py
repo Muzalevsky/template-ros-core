a3f6346a4f5b873f385d669dc74544912372694b
Muzalevsky <muzal2010@yandex.ru>
2021-10-23 14:47:40 +0300

    Update circle_drive.py

--- packages/circle_drive/scripts/circle_drive.py
+++ packages/circle_drive/scripts/circle_drive.py
@@ -22,11 +22,11 @@ class MyNode(DTROS):
         super(MyNode, self).__init__(node_name=node_name, node_type=NodeType.DEBUG)
         self.pub = rospy.Publisher("~car_cmd", Twist2DStamped, queue_size=1)
         self.subs = rospy.Subscriber("~car_cmd", Twist2DStamped, queue_size=1)
-        self.cam_subs = rospy.Subscriber("~car_cam", CompressedImage, camCallback)
+        self.cam_subs = rospy.Subscriber("~car_cam", CompressedImage, self.camCallback)
 
 
         self.log_pub = rospy.Publisher("SelfLog", String)        
-        self.log_subs = rospy.Subscriber("SelfLog", String, cbLogger)        
+        self.log_subs = rospy.Subscriber("SelfLog", String, self.cbLogger)        
         
     def run(self):
         # publish message every 1 second
