c73562b4254107caea1fef4711df428241d2b774
Muzalevsky <muzal2010@yandex.ru>
2021-10-23 13:14:41 +0300

    Update circle_drive.py

--- packages/circle_drive/scripts/circle_drive.py
+++ packages/circle_drive/scripts/circle_drive.py
@@ -2,11 +2,19 @@
 import rospy
 from duckietown.dtros import DTROS, NodeType
 from duckietown_msgs.msg import Twist2DStamped
+from duckietown_msgs.msg import CompressedImage
 from SearchCenterMarks import SearchMarks
 
 
 
 class MyNode(DTROS):    
+    def camCallback(image):
+            # Contour reading
+            frame = image
+            mark_road = SearchMarks(frame,0,0)
+            result_img, alpha, speed = mark_road.search_contours()
+            log_pub.publish("Frame:", alpha )            
+
     def cbLogger(string):
         rospy.loginfo("Publishing:" + string)
     
@@ -14,7 +22,7 @@ class MyNode(DTROS):
         super(MyNode, self).__init__(node_name=node_name, node_type=NodeType.DEBUG)
         self.pub = rospy.Publisher("~car_cmd", Twist2DStamped, queue_size=1)
         self.subs = rospy.Subscriber("~car_cmd", Twist2DStamped, queue_size=1)
-        self.subs = rospy.Subscriber("~car_cmd", Twist2DStamped, queue_size=1)
+        self.cam_subs = rospy.Subscriber("~car_cam", CompressedImage, camCallback)
 
 
         log_pub = rospy.Publisher("SelfLog", String)        
@@ -37,11 +45,7 @@ class MyNode(DTROS):
             self.pub.publish(msg)
             log_pub.publish("Not yet...") 
             
-            # Contour reading
-            ret, frame = cap.read()
-            mark_road = SearchMarks(frame,0,0)
-            result_img, alpha, speed = mark_road.search_contours()
-            
+
             rate.sleep()
 
 if __name__ == '__main__':
