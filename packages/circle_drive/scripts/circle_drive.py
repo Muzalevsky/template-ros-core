609b9d114115c64b8b6675381c5d088a0ca5cd85
Muzalevsky <muzal2010@yandex.ru>
2021-10-25 17:54:26 +0300

    Update circle_drive.py

--- packages/circle_drive/scripts/circle_drive.py
+++ packages/circle_drive/scripts/circle_drive.py
@@ -8,16 +8,16 @@ from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge
 #from SearchCenterMarks import SearchMarks
 
-
-
+   
 class MyNode(DTROS):    
     def camCallback(self,image):
         rospy.loginfo(f"Frame: {0}")
-        # Contour reading
-        frame = image.data
+        cv_image = self.bridge.imgmsg_to_cv2(image, desired_encoding='passthrough')
+        height, width = cv_image.shape[:2]
+        rospy.loginfo(f"Frame: {height, width}")
+        
         #mark_road = SearchMarks(frame,0,0)
         #result_img, alpha, speed = mark_road.search_contours()
-        rospy.loginfo(f"Frame: {0}")
             
     def cbLogger(self,string):
         rospy.loginfo(f"Publishing: {string}")
@@ -27,7 +27,8 @@ class MyNode(DTROS):
         self.pub = rospy.Publisher("~car_cmd", Twist2DStamped, queue_size=1)
         self.subs = rospy.Subscriber("~car_cmd", Twist2DStamped, queue_size=1)
         self.cam_subs = rospy.Subscriber("/autobot08/camera_node/image/compressed", CompressedImage, self.camCallback)
-        
+        self.bridge = CvBridge()
+
     def run(self):
         # publish message every 1 second
         rate = rospy.Rate(3) # 1Hz
